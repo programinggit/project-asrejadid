ff4cd71f7f6ed359938cc7a3d320972ee3234af8
elham <khorsandielham1gmail.com>
2022-04-06 16:59:02 +0430

    Computation model is here

--- web/models.py
+++ web/models.py
@@ -1,3 +1,10 @@
+from email.policy import default
+from time import timezone
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class  Computetion (models.Model):
+    image = models.ImageField(upload_to = "image_uploaded", null = True, blank = True)
+    video = models.FileField(upload_to = "video_uploaded", null = True)
+    date_uploaded = models.DateTimeField()
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
